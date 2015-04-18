82a9c2028684be60b18b1bf12f4b1304e5b2e993
phazyy <phazyy9@gmail.com>
2015-04-18 13:40:56 +0100

    Inital commit #2
    Adding the first page (index.html)

---
+++ scripts/portfolio.js
@@ -0,0 +1,24 @@
+
+$(window).scroll( function(){
+    // Change Navbar styling after scrolling
+    if ($(".navbar-custom").offset().top > 150) {
+        $(".navbar-custom").addClass("navbar-fixed-collapse");
+    }
+    else {
+        $(".navbar-custom").removeClass("navbar-fixed-collapse");
+    }
+
+    // Reveal Screen Images after scrolling
+
+    $('.section img').each( function(i) {
+        var imgY = $(this).offset().top + $(this).outerHeight();
+        var windowY = $(window).scrollTop() + $(window).height();
+        if (windowY > imgY) {
+            $(this).removeClass("screens-collapse");
+        }
+        else {
+            $(this).addClass("screens-collapse");
+        }
+    });
+
+});

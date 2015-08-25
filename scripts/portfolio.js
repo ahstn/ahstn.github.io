c1f98a8d23a26d51a4f088d5a860474bcd377556
Adam Houston <phazyy9@gmail.com>
2015-08-25 20:45:26 +0100

    Updating project descriptions and minor style tweaks

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -10,8 +10,11 @@ $(window).scroll( function(){
 
     // Reveal Screen Images after scrolling
     $('.section img').each( function(i) {
-        var imgY = $(this).offset().top + $(this).outerHeight();
+        // Locate the image position (Y coord) and offset it by 300
+        var imgY = ($(this).offset().top + $(this).outerHeight()) - 300;
+        // Window position (Y coord) relative to the page scroll
         var windowY = $(window).scrollTop() + $(window).height();
+        // If the window has passed the imgY position, reveal the image
         if (windowY > imgY) {
             $(this).removeClass("screens-collapse");
         }

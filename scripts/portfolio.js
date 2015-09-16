32f4c127958d3e9e89f87f4f29e69328e070b46e
Adam Houston <phazyy9@gmail.com>
2015-09-16 11:36:17 +0100

    Added content reveal animations to all pages

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,28 +1,29 @@
+$(document).ready(function(){
+    // overflow-x breaks $(window).scroll
+    $('body').scroll(function(event){
+        $('section').each( function() {
+            // Locate the section position and
+            // add an offset based on the window height
+            var sectionY = $(this).offset().top + ($(window).height() / 5);
+            // Window position (Y coord) relative to the page scroll
+            var windowY = $(window).scrollTop() + $(window).height();
+            // If the window has passed the sectionY position, reveal the image
+            if (windowY > sectionY) {
+                $(this).addClass("animate-in");
+            }
+        });
 
-$(window).scroll( function(){
-    // Change Navbar styling after scrolling
-    if ($(".navbar-custom").offset().top > 150) {
-        $(".navbar-custom").addClass("navbar-fixed-collapse");
-    }
-    else {
-        $(".navbar-custom").removeClass("navbar-fixed-collapse");
-    }
-
-    // Reveal Screen Images after scrolling
-    $('.section img').each( function(i) {
-        // Locate the image position (Y coord) and offset it by 300
-        var imgY = ($(this).offset().top + $(this).outerHeight()) - 300;
-        // Window position (Y coord) relative to the page scroll
-        var windowY = $(window).scrollTop() + $(window).height();
-        // If the window has passed the imgY position, reveal the image
-        if (windowY > imgY) {
-            $(this).removeClass("screens-collapse");
+        // Change Navbar styling after scrolling
+        if ($('.navbar-custom').offset().top > 150) {
+            $('.navbar-custom').addClass('navbar-fixed-collapse');
+        }
+        else {
+            $('.navbar-custom').removeClass('navbar-fixed-collapse');
         }
     });
 
-});
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 });
